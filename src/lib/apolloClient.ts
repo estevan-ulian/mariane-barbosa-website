ecfa5ad4683dfa8b69a188b40b0feba958b6fc31
Estevan Ulian <estevan.ulian@gmail.com>
2022-07-19 11:42:14 -0300

    Update

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: process.env.GRAPHQL_ENDPOINT,
+    uri: `${process.env.GRAPHQL_ENDPOINT}/master`,
     cache: new InMemoryCache(),
   });
 
